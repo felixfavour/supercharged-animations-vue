e161ed046f012edf2412deaf8ac5bbdbf83f4ab0
felixfavour <felixfavour0@gmail.com>
2023-09-28 16:16:04 +0100

    feat: completed UI for operation

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,103 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import MovieCard from './components/MovieCard.vue';
+import movies from './assets/movies'
+import { ref } from 'vue';
+
+const query = ref('')
+const searchQuery = ref('')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <header>
+      <h1>Movie Grid</h1>
+      <input type="search" v-model="searchQuery" placeholder="Search Movies">
+      <div class="button-group">
+        <button @click="query = '2021'" :class="{ active: query === '2021' }">2021</button>
+        <button @click="query = '2022'" :class="{ active: query === '2022' }">2022</button>
+        <button @click="query = 'Action'" :class="{ active: query === 'Action' }">Action</button>
+        <button @click="query = 'Comedy'" :class="{ active: query === 'Comedy' }">Comedy</button>
+        <button @click="query = 'Horror'" :class="{ active: query === 'Horror' }">Horror</button>
+        <button @click="query = 'Romance'" :class="{ active: query === 'Romance' }">Romance</button>
+        <button @click="query = 'Adventure'" :class="{ active: query === 'Adventure' }">Adventure</button>
+        <button @click="query = 'Documentary'" :class="{ active: query === 'Documentary' }">Documentary</button>
+      </div>
+    </header>
+    <div class="movie-grid">
+      <MovieCard v-for="movie in movies" :key="movie.href" :movie="movie" />
+    </div>
   </main>
 </template>
 
 <style scoped>
+main {
+  text-align: center;
+  max-width: 800px;
+  align-items: center;
+  margin: 0 auto;
+}
+
 header {
-  line-height: 1.5;
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+input {
+  height: 50px;
+  border-radius: 6px;
+  border: 1px solid rgba(2, 28, 62, .1);
+  font-size: 1rem;
+  padding: 0 1rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.button-group {
+  display: -webkit-box;
+  max-width: 100%;
+  overflow: auto;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.button-group button {
+  font-size: 1rem;
+  height: 40px;
+  display: grid;
+  place-items: center;
+  margin-right: 0.4rem;
+  padding: 0 1rem;
+  border: 1px solid #ffab2d;
+  border-radius: 6px;
+  color: #ffab2d;
+  background: #fffbf4;
+  font-weight: 600;
+  cursor: pointer;
+  transition: 0.3s;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+.button-group button.active {
+  background: #ffab2d;
+  color: #fffbf4;
+}
+
+h1 {
+  font-weight: 700;
+}
+
+.movie-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem 1rem;
+}
+
+@media screen and (max-width: 1024px) {
+  .movie-grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .movie-grid {
+    grid-template-columns: repeat(1, 1fr);
   }
 }
 </style>
